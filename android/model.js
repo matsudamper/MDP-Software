5fc50e7eac137d5d2c106ba042cb478a7094e00f
matsudamper <matsudamper971227@gmail.com>
2017-10-11 12:05:28 +0900

    カテゴリ分け実装

--- android/model.js
+++ android/model.js
@@ -1,83 +1,121 @@
 var products = {};
 new Vue({
-    el: '#products',
+    el: '#categories',
     data: {
-        items: [
+        categories: [
             {
-                name: 'SmallMemo',
-                dir: 'smallmemo',
-                url: 'https://play.google.com/store/apps/details?id=com.matsudamper.smallmemopad'
+                title: "SONY Small Application",
+                dir: "SONYSmallApp",
+                items: [
+                    {
+                        name: 'SmallMemo',
+                        dir: 'smallmemo',
+                        url: 'https://play.google.com/store/apps/details?id=com.matsudamper.smallmemopad'
+                    },
+                    {
+                        name: 'SmallTweet',
+                        dir: 'SmallTweet',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.smalltweet'
+                    },
+                    {
+                        name: 'SmallTimer',
+                        dir: 'SmallTimer',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.smalltimer'
+                    },
+                    {
+                        name: 'Small Lamp',
+                        dir: 'SmallLamp',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.smalllamp'
+                    },
+                    {
+                        name: 'Small Tube',
+                        dir: 'SmallTube',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.smalltube'
+                    },
+                    {
+                        name: 'Small nico',
+                        dir: 'Small2525',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.small2525'
+                    }
+                ]
             },
             {
-                name: 'SmallTweet',
-                dir: 'SmallTweet',
-                url: 'https://play.google.com/store/apps/details?id=net.matsudamper.smalltweet'
+                title: "Small Application for All Device",
+                dir: "SmallAppForAllDevice",
+                items: [
+                    {
+                        name: 'Small Tube for All Device',
+                        dir: 'SmallTube',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.smallapp.smalltube'
+                    },
+                    {
+                        name: 'Small Timer for All Device',
+                        dir: 'SmallTimer',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.smallapp.smalltimer'
+                    },
+                    {
+                        name: 'Small Lamp for All Device',
+                        dir: 'SmallLamp',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.smallapp.smalllamp'
+                    },
+                    {
+                        name: 'Small Browser for All Device',
+                        dir: 'SmallBrowser',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.smallapp.smallbrowser'
+                    }
+                ]
             },
             {
-                name: 'One Mio',
-                dir: 'OneMio',
-                url: 'https://play.google.com/store/apps/details?id=com.matsudamper.onemio'
-            },
-            {
-                name: 'Tweet Bookark',
-                dir: 'TweetBookark',
-                url: 'https://play.google.com/store/apps/details?id=com.matsudamper.TweetBookmark'
-            },
-            {
-                name: 'Rotate Screenshot',
-                dir: 'RotateScreenshot',
-                url: 'https://play.google.com/store/apps/details?id=net.matsudamper.rotatescreenshot'
-            },
-            {
-                name: 'PhotoEditRotate',
-                dir: 'PhotoEditRotate',
-                url: 'https://play.google.com/store/apps/details?id=net.matsudamper.photorotate'
-            },
-            {
-                name: 'SmallTimer',
-                dir: 'SmallTimer',
-                url: 'https://play.google.com/store/apps/details?id=net.matsudamper.smalltimer'
-            },
-            {
-                name: 'ふりったー',
-                dir: 'Flitter',
-                url: 'https://play.google.com/store/apps/details?id=net.matsudamper.kanon'
-            },
-            {
-                name: 'HORIKAESHI',
-                dir: 'HORIKAESHI',
-                url: 'https://play.google.com/store/apps/details?id=net.matsudamper.horikaeshi'
-            },
-            {
-                name: 'Small Lamp',
-                dir: 'SmallLamp',
-                url: 'https://play.google.com/store/apps/details?id=net.matsudamper.smalllamp'
-            },
-            {
-                name: 'Small Tube',
-                dir: 'SmallTube',
-                url: 'https://play.google.com/store/apps/details?id=net.matsudamper.smalltube'
-            },
-            {
-                name: '淀橋',
-                dir: 'Yodobashi',
-                url: 'https://play.google.com/store/apps/details?id=net.matsudamper.yodobashi'
-            },
-            {
-                name: 'Small nico',
-                dir: 'Small2525',
-                url: 'https://play.google.com/store/apps/details?id=net.matsudamper.small2525'
-            },
-            {
-                name: 'プリセットカレンダー',
-                dir: 'PresetCalendar',
-                url: 'https://play.google.com/store/apps/details?id=net.matsudamper.presetcalendar'
+                title: "Other Products",
+                dir: "Others",
+                items: [
+                    {
+                        name: 'One Mio',
+                        dir: 'OneMio',
+                        url: 'https://play.google.com/store/apps/details?id=com.matsudamper.onemio'
+                    },
+                    {
+                        name: 'Tweet Bookark',
+                        dir: 'TweetBookark',
+                        url: 'https://play.google.com/store/apps/details?id=com.matsudamper.TweetBookmark'
+                    },
+                    {
+                        name: 'Rotate Screenshot',
+                        dir: 'RotateScreenshot',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.rotatescreenshot'
+                    },
+                    {
+                        name: 'PhotoEditRotate',
+                        dir: 'PhotoEditRotate',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.photorotate'
+                    },
+                    {
+                        name: 'ふりったー',
+                        dir: 'Flitter',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.kanon'
+                    },
+                    {
+                        name: 'HORIKAESHI',
+                        dir: 'HORIKAESHI',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.horikaeshi'
+                    },
+                    {
+                        name: '淀橋',
+                        dir: 'Yodobashi',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.yodobashi'
+                    },
+                    {
+                        name: 'プリセットカレンダー',
+                        dir: 'PresetCalendar',
+                        url: 'https://play.google.com/store/apps/details?id=net.matsudamper.presetcalendar'
+                    }
+                ]
             }
         ]
     },
     methods: {
-        icon: function (dir) {
-            return dir + "\\icon.png";
+        icon: function (category_dir, dir) {
+            return category_dir + "\\" + dir + "\\icon.png";
         }
     }
 });
